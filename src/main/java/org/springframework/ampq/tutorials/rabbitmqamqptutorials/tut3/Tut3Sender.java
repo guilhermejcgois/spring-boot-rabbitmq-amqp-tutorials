33d0a0eecd6617f12e9469f567539285b6f46eec
Guilherme Gois <guijocargo@gmail.com>
2023-01-24 12:48:16 -0300

    feat: adds fanout example

---
+++ src/main/java/org/springframework/ampq/tutorials/rabbitmqamqptutorials/tut3/Tut3Sender.java
@@ -0,0 +1,32 @@
+package org.springframework.ampq.tutorials.rabbitmqamqptutorials.tut3;
+
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Tut3Sender {
+    AtomicInteger dots = new AtomicInteger(0);
+    AtomicInteger count = new AtomicInteger(0);
+    @Autowired
+    private RabbitTemplate template;
+    @Autowired
+    private FanoutExchange fanout;
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 500)
+    public void send() {
+        StringBuilder builder = new StringBuilder("Hello");
+        if (dots.getAndIncrement() == 3) {
+            dots.set(1);
+        }
+        for (int i = 0; i < dots.get(); i++) {
+            builder.append('.');
+        }
+        builder.append(count.incrementAndGet());
+        String message = builder.toString();
+        template.convertAndSend(fanout.getName(), "", message);
+        System.out.println(" [x] Sent '" + message + "'");
+    }
+}
