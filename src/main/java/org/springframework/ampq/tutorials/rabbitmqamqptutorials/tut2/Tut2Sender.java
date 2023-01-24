038aa3899f918520b9305958e9badc9216aa2107
Guilherme Gois <guijocargo@gmail.com>
2023-01-23 23:53:16 -0300

    feat: creates multiconsumer package

---
+++ src/main/java/org/springframework/ampq/tutorials/rabbitmqamqptutorials/tut2/Tut2Sender.java
@@ -0,0 +1,32 @@
+package org.springframework.ampq.tutorials.rabbitmqamqptutorials.tut2;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Tut2Sender {
+    AtomicInteger dots = new AtomicInteger(0);
+    AtomicInteger count = new AtomicInteger(0);
+    @Autowired
+    private RabbitTemplate template;
+    @Autowired
+    private Queue queue;
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 500)
+    public void send() {
+        StringBuilder builder = new StringBuilder("Hello");
+        if (dots.incrementAndGet() == 4) {
+            dots.set(1);
+        }
+        for (int i = 0; i < dots.get(); i++) {
+            builder.append('.');
+        }
+        builder.append(count.incrementAndGet());
+        String message = builder.toString();
+        template.convertAndSend(queue.getName(), message);
+        System.out.println(" [x] Sent '" + message + "'");
+    }
+}
