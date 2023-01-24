5ef3f9cbbbea6030a257883909203161c493bd6d
Guilherme Gois <guijocargo@gmail.com>
2023-01-23 22:59:07 -0300

    feat: create repo with simple producer and consumers

---
+++ src/test/java/org/springframework/ampq/tutorials/rabbitmqamqptutorials/RabbitmqAmqpTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.ampq.tutorials.rabbitmqamqptutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqAmqpTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
