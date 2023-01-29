22f0748aace80d483159a7b339217699b995777c
Guilherme Gois <guijocargo@gmail.com>
2023-01-29 11:52:25 -0300

    feat: creates routing package

---
+++ src/main/java/org/springframework/ampq/tutorials/rabbitmqamqptutorials/tut4/Tut4Sender.java
@@ -0,0 +1,38 @@
+package org.springframework.ampq.tutorials.rabbitmqamqptutorials.tut4;
+
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Tut4Sender {
+
+    @Autowired
+    private RabbitTemplate template;
+
+    @Autowired
+    private DirectExchange direct;
+
+    AtomicInteger index = new AtomicInteger(0);
+
+    AtomicInteger count = new AtomicInteger(0);
+
+    private final String[] keys = {"orange", "black", "green"};
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 500)
+    public void send() {
+        StringBuilder builder = new StringBuilder("Hello to ");
+        if (this.index.incrementAndGet() == 3) {
+            this.index.set(0);
+        }
+        String key = keys[this.index.get()];
+        builder.append(key).append(' ');
+        builder.append(this.count.get());
+        String message = builder.toString();
+        template.convertAndSend(direct.getName(), key, message);
+        System.out.println(" [x] Sent '" + message + "'");
+    }
+
+}
